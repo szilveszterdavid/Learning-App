9b4a96af17f67d84d2671b4ab86ae6ada7a8eaab
Szilveszter Dávid <szilveszterdavid@yahoo.com>
2021-10-06 22:53:31 +0300

    Json Decoder

--- ViewModels/ContentModel.swift
+++ ViewModels/ContentModel.swift
@@ -9,4 +9,34 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        // Get a url to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            // Read the file into a data object
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            // Try to decode the json file into an array of modues
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            // Assign parsed modules to modules property
+            self.modules = modules
+        }
+        catch {
+            
+            // Log error
+            print(error)
+        }
+    }
+    
 }
