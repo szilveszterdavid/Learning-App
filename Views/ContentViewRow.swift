953dacfa0dc59abeb0224de15b90f7daa8734d81
Szilveszter Dávid <szilveszterdavid@yahoo.com>
2021-10-07 21:17:24 +0300

    Commit everything

---
+++ Views/ContentViewRow.swift
@@ -0,0 +1,20 @@
+//
+//  ContentViewRow.swift
+//  Learning App
+//
+//  Created by Szilveszter Dávid on 07.10.2021.
+//
+
+import SwiftUI
+
+struct ContentViewRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentViewRow()
+    }
+}
