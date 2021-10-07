5ee03615d972cd02517346811ff090fd73b3657f
Szilveszter Dávid <szilveszterdavid@yahoo.com>
2021-10-07 21:25:49 +0300

    ContentViewRow

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -22,33 +22,7 @@ struct ContentView: View {
                     
                     ForEach(0..<model.currentModule!.content.lessons.count) { index in
                         
-                        let lesson = model.currentModule!.content.lessons[index]
-                        
-                        // Lesson card
-                        ZStack (alignment: .leading) {
-                            
-                            Rectangle()
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                                .shadow(radius: 5)
-                                .frame(height: 66)
-                            
-                            HStack (spacing: 30) {
-                                
-                                Text(String(index + 1))
-                                    .bold()
-                                
-                                VStack (alignment: .leading){
-                                    Text(lesson.title)
-                                        .bold()
-                                    Text(lesson.duration)
-                                }
-                                
-                            }
-                            .padding()
-                            
-                        }
-                        .padding(.bottom, 10)
+                        ContentViewRow(index: index)
                         
                     }
                 }
--- Views/ContentViewRow.swift
+++ Views/ContentViewRow.swift
@@ -8,13 +8,39 @@
 import SwiftUI
 
 struct ContentViewRow: View {
+    
+    @EnvironmentObject var model: ContentModel
+    var index: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        
+        let lesson = model.currentModule!.content.lessons[index]
+        
+        // Lesson card
+        ZStack (alignment: .leading) {
+            
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .frame(height: 66)
+            
+            HStack (spacing: 30) {
+                
+                Text(String(index + 1))
+                    .bold()
+                
+                VStack (alignment: .leading){
+                    Text(lesson.title)
+                        .bold()
+                    Text(lesson.duration)
+                }
+                
+            }
+            .padding()
+            
+        }
+        .padding(.bottom, 2)
 
-struct ContentViewRow_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentViewRow()
     }
 }
