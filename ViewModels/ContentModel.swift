f82787f159b0f3d27e759240037c79941841c87c
Szilveszter Dávid <szilveszterdavid@yahoo.com>
2021-10-10 13:28:14 +0300

    Description - HTML Scroll back to the top

--- ViewModels/ContentModel.swift
+++ ViewModels/ContentModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreData
 
 class ContentModel: ObservableObject {
     
@@ -19,6 +20,9 @@ class ContentModel: ObservableObject {
     // Current lesson
     @Published var currentLesson: Lesson?
     var currentLessonIndex = 0
+    
+    // Current lesson explanation
+    @Published var lessonDescription = NSAttributedString()
     
     var styleData: Data?
     
@@ -95,6 +99,7 @@ class ContentModel: ObservableObject {
         
         // Set the current lesson
         currentLesson = currentModule!.content.lessons[currentLessonIndex]
+        lessonDescription = addStyling(currentLesson!.explanation)
     }
     
     func nextLesson() {
@@ -107,6 +112,7 @@ class ContentModel: ObservableObject {
             
             // Set the current lesson property
             currentLesson = currentModule!.content.lessons[currentLessonIndex]
+            lessonDescription = addStyling(currentLesson!.explanation)
         }
         else {
             
@@ -119,6 +125,31 @@ class ContentModel: ObservableObject {
     func hasNextLesson() -> Bool {
         
         return (currentLessonIndex + 1 < currentModule!.content.lessons.count)
+    }
+    
+    // MARK: - Code Styling
+    private func addStyling(_ htmlString: String) -> NSAttributedString {
+        
+        var resultString = NSAttributedString()
+        var data = Data()
+        
+        // Add the styling data
+        if styleData != nil {
+            data.append(styleData!)
+        }
+        
+        // Add the html data
+        data.append(Data(htmlString.utf8))
+        
+        // Convert to attributed string
+        if let attributedString = try? NSAttributedString(data: data, options: [.documentType:
+            NSAttributedString.DocumentType.html], documentAttributes: nil) {
+            
+            resultString = attributedString
+        }
+        
+        return resultString
+        
     }
     
 }
--- Views/CodeTextView.swift
+++ Views/CodeTextView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CodeTextView: UIViewRepresentable {
+    
+    @EnvironmentObject var model: ContentModel
 
     func makeUIView(context: Context) -> UITextView {
         let textView = UITextView()
@@ -19,9 +21,10 @@ struct CodeTextView: UIViewRepresentable {
     func updateUIView(_ textView: UITextView, context: Context) {
         
         // Set the attributed text for the lesson
-        textView.text = "Testing"
+        textView.attributedText = model.lessonDescription
         
         // Scroll back to the top
+        textView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: false)
     }
     
 }
