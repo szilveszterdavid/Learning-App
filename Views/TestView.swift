4ed2bebc141d2f9635f25d3172ee65bbd963c2ab
Szilveszter Dávid <szilveszterdavid@yahoo.com>
2021-10-11 21:23:29 +0300

    Buttons

--- Views/TestView.swift
+++ Views/TestView.swift
@@ -10,23 +10,77 @@ import SwiftUI
 struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
+    @State var selectedAnswerIndex: Int?
+    @State var numCorrect = 0
+    @State var submitted = false
     
     var body: some View {
         
         if model.currentQuestion != nil {
             
-            VStack {
+            VStack (alignment: .leading) {
                 
                 // Question number
                 Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
                 
                 // Question
                 CodeTextView()
+                    .padding(.horizontal, 20)
                 
                 // Answers
                 
-                // Button
+                ScrollView {
+                    
+                    VStack {
+                        
+                        ForEach (0..<model.currentQuestion!.answers.count, id: \.self) { index in
+                            
+                            Button {
+                                // Track the selected index
+                                selectedAnswerIndex = index
+                                
+                            } label : {
+                                
+                                ZStack {
+                                    
+                                    RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                        .frame(height: 48)
+                                    Text(model.currentQuestion!.answers[index])
+                                }
+                            }
+                            .disabled(submitted)
+                        }
+                    }
+                    .accentColor(.black)
+                    .padding()
+                }
                 
+                // Button
+                Button {
+                    
+                    // Change submitted state to true
+                    submitted = true
+                    
+                    // Check the answer and increment the counter if correct
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                    numCorrect += 1
+                    }
+                    
+                } label: {
+                    
+                    ZStack {
+                        
+                        RectangleCard(color: .green)
+                            .frame(height: 48)
+                        Text("Submit")
+                            .bold()
+                            .foregroundColor(Color.white)
+                    }
+                    .padding()
+                    
+                }
+                .disabled(selectedAnswerIndex == nil)
             }
             .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
         }
