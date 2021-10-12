768dc4561597e3514997ad929136ff2e6dc009d2
Szilveszter Dávid <szilveszterdavid@yahoo.com>
2021-10-12 20:09:07 +0300

    Data from GitHub

--- ViewModels/ContentModel.swift
+++ ViewModels/ContentModel.swift
@@ -35,7 +35,12 @@ class ContentModel: ObservableObject {
     var styleData: Data?
     
     init() {
+        
+        // Parse local included json data
         getLocalData()
+        
+        // Download remote json file and parse data
+        getRemoteData()
     }
     
     // MARK: - Data methods
@@ -75,6 +80,55 @@ class ContentModel: ObservableObject {
         catch {
             print(error)
         }
+    }
+    
+    func getRemoteData() {
+        
+        // String path
+        let urlString = "https://szilveszterdavid.github.io/Learning-App-Data-Json/data2.json"
+        
+        // Create a url object
+        let url = URL(string: urlString)
+        
+        guard url != nil else {
+            
+            // Couldn't create url
+            return
+        }
+        
+        // Create a URLRequest object
+        let request = URLRequest(url: url!)
+        
+        // Get the session and kick off the task
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            
+            // Check if there's an error
+            guard error == nil else {
+                
+                // There was an error
+                return
+            }
+            
+            do {
+                // Create json decoder
+                let decoder = JSONDecoder()
+                
+                // Decode
+                let modules = try decoder.decode([Module].self, from: data!)
+                
+                // Append parsed modules into modules property
+                self.modules += modules
+            }
+            catch {
+                // Couldn't parse the json
+                print(error)
+            }
+        }
+        
+        // Kick off data task
+        dataTask.resume()
     }
     
     // MARK: - Module navigation methods
